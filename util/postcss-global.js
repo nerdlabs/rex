3fcae6564e77c520aa95f1959565f67483213470
dmbch <daniel@dmbch.net>
2016-01-27 19:17:06 +0100

    introduce custom global styles

--- util/postcss-global.js
+++ util/postcss-global.js
@@ -7,8 +7,13 @@ var normalizePath = require.resolve('normalize.css');
 var normalizeCSS = fs.readFileSync(normalizePath);
 var normalizeAST = postcss.parse(normalizeCSS, { from: normalizePath });
 
-module.exports = postcss.plugin('normalize', function () {
+var globalPath = require.resolve('./global.css');
+var globalCSS = fs.readFileSync(globalPath);
+var globalAST = postcss.parse(globalCSS, { from: globalPath });
+
+module.exports = postcss.plugin('global', function () {
   return function (css) {
+    css.prepend(globalAST);
     css.prepend(normalizeAST);
   };
 });
