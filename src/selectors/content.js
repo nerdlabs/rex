757c25b39dbb432b97f13595f616a5c54c423208
dmbch <daniel@dmbch.net>
2016-01-28 16:56:23 +0100

    fix formatting

--- src/selectors/content.js
+++ src/selectors/content.js
@@ -2,11 +2,11 @@
 import { createSelector } from 'reselect';
 
 export const homeContentSelector = createSelector(
-  (state) => state.content.home,
-  (content) => ({ content })
+  state => state.content.home,
+  content => ({ content })
 );
 
 export const aboutContentSelector = createSelector(
-  (state) => state.content.about,
-  (content) => ({ content })
+  state => state.content.about,
+  content => ({ content })
 );
